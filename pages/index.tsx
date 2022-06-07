8ea03f8ee91f50c857ee882e07a2a4eb02d0fc71
balbboa <arthur.balboa.108@ufrn.edu.br>
2022-06-07 00:38:23 -0300

    Feat: wallpaper

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Entry() {
   return (
     <Container title="Filminho">
     <div className="flex flex-col w-full h-full">
-      <div className="flex flex-col align-middle items-center justify-center my-28 mx-auto">
+      <div className="flex flex-col align-middle items-center justify-center my-20 mx-auto">
         <img
           src="movie.png"
           className='w-64'
@@ -21,7 +21,7 @@ export default function Entry() {
 
         <Link href="/home">
           <button
-            className="text-white font-medium p-0 w-16 h-16 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
+            className="text-white font-medium p-0 w-20 h-20 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
           >
             Lesgo!
           </button>
