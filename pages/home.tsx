dc5b6474a06a96bd98c5b11f52f0fe8906292c59
balbboa <abmm_arthur@hotmail.com>
2022-06-14 10:30:58 -0300

    Feat: sobre

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,107 +1,132 @@
 /* eslint-disable @next/next/no-img-element */
-import { Dialog, Transition } from '@headlessui/react';
-import Link from 'next/link';
-import { Fragment, useState } from 'react';
-import Container from '../components/Container';
+import { Dialog, Transition } from "@headlessui/react";
+import Link from "next/link";
+import { Fragment, useState } from "react";
+import Container from "../components/Container";
 
 export default function Home() {
-  let cont = 0
-
-  let movieSearchBox: null | any = ''
-  let searchList: any = ''
+  let movieSearchBox: null | any = "";
+  let searchList: any = "";
 
   if (typeof document !== "undefined") {
-    movieSearchBox = (document.getElementById('idfilme') as HTMLInputElement)
-    searchList = (document.getElementById('search-list') as HTMLInputElement)
+    movieSearchBox = document.getElementById("idfilme") as HTMLInputElement;
+    searchList = document.getElementById("search-list") as HTMLInputElement;
   }
 
-  const Foto = '1.jpeg';
-  const Foto1 = '2.jpeg';
-  const Foto2 = '3.jpeg';
-  const Foto3 = '4.jpeg';
-  const Foto4 = '5.jpeg';
-  const Foto5 = '6.jpeg';
+  const Foto = "1.jpeg";
+  const Foto1 = "2.jpeg";
+  const Foto2 = "3.jpeg";
+  const Foto3 = "4.jpeg";
+  const Foto4 = "5.jpeg";
+  const Foto5 = "6.jpeg";
 
-  const [isOpen, setIsOpen] = useState<any>(false)
-  const [changeImg, setChangeImg] = useState<any>(0)
-  const [foto1, setFoto1] = useState<any>('hidden')
-  const [foto2, setFoto2] = useState<any>('hidden')
-  const [foto3, setFoto3] = useState<any>('hidden')
-  const [foto4, setFoto4] = useState<any>('hidden')
-  const [foto5, setFoto5] = useState<any>('hidden')
-  const [state, setState] = useState<boolean>()
+  const [isOpen, setIsOpen] = useState<any>(false);
+  const [changeImg, setChangeImg] = useState<any>(0);
+  const [foto1, setFoto1] = useState<any>("hidden");
+  const [foto2, setFoto2] = useState<any>("hidden");
+  const [foto3, setFoto3] = useState<any>("hidden");
+  const [foto4, setFoto4] = useState<any>("hidden");
+  const [foto5, setFoto5] = useState<any>("hidden");
+  const [state, setState] = useState<boolean>();
 
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
 
   function contImg() {
-    setChangeImg(changeImg + 1)
+    setChangeImg(changeImg + 1);
   }
 
   const callImg = async (e: any) => {
     e.preventDefault();
-    cont++
-    sessionStorage.setItem(`try${[cont]}`, e.target.elements.filme.value)
 
-    if (e.target.elements.filme.value !== 'The Big Lebowski') { await contImg() }
-    else {
-      openModal()
-      setState(true)
-      setFoto1('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
-      setFoto2('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
-      setFoto3('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
-      setFoto4('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
-      setFoto5('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+    if (e.target.elements.filme.value !== "The Big Lebowski") {
+      await contImg();
+    } else {
+      openModal();
+      setState(true);
+      setFoto1(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
+      setFoto2(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
+      setFoto3(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
+      setFoto4(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
+      setFoto5(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
     }
 
     if (changeImg == 0) {
-      setFoto1('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+      sessionStorage.setItem("try 1:", e.target.elements.filme.value);
+      setFoto1(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
     }
     if (changeImg == 1) {
-      setFoto2('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+      sessionStorage.setItem("try 2:", e.target.elements.filme.value);
+      setFoto2(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
     }
     if (changeImg == 2) {
-      setFoto3('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+      sessionStorage.setItem("try 3:", e.target.elements.filme.value);
+      setFoto3(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
     }
     if (changeImg == 3) {
-      setFoto4('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+      sessionStorage.setItem("try 4:", e.target.elements.filme.value);
+      setFoto4(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
     }
     if (changeImg == 4) {
-      setFoto5('cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm')
+      sessionStorage.setItem("try 5:", e.target.elements.filme.value);
+      setFoto5(
+        "cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
+      );
+    }
+    if (changeImg == 5) {
+      sessionStorage.setItem("try 6:", e.target.elements.filme.value);
     }
     if (changeImg > 5) {
-      setState(false)
-      openModal()
+      setState(false);
+      openModal();
     }
-    searchList.classList.remove('hidden');
-  }
+    searchList.classList.remove("hidden");
+  };
 
   async function loadMovies(searchMovie: string) {
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=0c7861c0f9f582ae2a9ca7772ea92061&query=${searchMovie}`
-    const res = await fetch(`${URL}`)
-    const data = await res.json()
-    listMovies(data.results)
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=0c7861c0f9f582ae2a9ca7772ea92061&query=${searchMovie}`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    listMovies(data.results);
   }
 
   function findMovies() {
-    let searchMovies = ''
+    let searchMovies = "";
     if (movieSearchBox) {
-      searchMovies = (movieSearchBox.value).trim()
-      loadMovies(searchMovies)
+      searchMovies = movieSearchBox.value.trim();
+      loadMovies(searchMovies);
     }
   }
 
   function listMovies(movies: string | any[]) {
-    searchList.innerHTML = ''
+    searchList.innerHTML = "";
     for (let i = 0; i < movies?.length; i++) {
-      let movieListItem = document.createElement('div');
-      movieListItem.dataset.id = movies[i].id; 
-      movieListItem.classList.add('search-list-item');
+      let movieListItem = document.createElement("div");
+      movieListItem.dataset.id = movies[i].id;
+      movieListItem.classList.add("search-list-item");
 
       movieListItem.innerHTML = `
       <div class="flex justify-between cursor-pointer bg-gray-700 border-gray-600 text-white hover:text-black hover:bg-yellow-100 px-2 py-2">
@@ -109,10 +134,10 @@ export default function Home() {
           <p>${movies[i].release_date}</p>
       </div>
       `;
-      movieListItem.addEventListener('click', async () => {
+      movieListItem.addEventListener("click", async () => {
         movieSearchBox.value = movies[i].title;
-        searchList.classList.add('hidden');
-      })
+        searchList.classList.add("hidden");
+      });
       searchList.appendChild(movieListItem);
     }
   }
@@ -121,20 +146,33 @@ export default function Home() {
     <Container title="Filminho">
       <div className="flex flex-col w-full h-full">
         <form className="mt-5 w-1/3 mx-auto" onSubmit={callImg}>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+          >
+            Search
+          </label>
           <div className="relative">
             <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <img
-                src={'search.png'}
-                className='w-4 h-4'
-                alt="..."
-              />
+              <img src={"search.png"} className="w-4 h-4" alt="..." />
             </div>
-            <input onKeyUp={findMovies} type="search" id="idfilme" name="filme" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-yellow-500 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500" placeholder="Digite o filme do dia" required />
-            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Xama</button>
+            <input
+              onKeyUp={findMovies}
+              type="search"
+              id="idfilme"
+              name="filme"
+              className="block p-4 pl-10 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-yellow-500 focus:border-yellow-500"
+              placeholder="Digite o filme do dia"
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800"
+            >
+              Xama
+            </button>
           </div>
-          <div className="text-sm w-1/3 absolute" id="search-list">
-          </div>
+          <div className="text-sm w-1/3 absolute" id="search-list"></div>
         </form>
 
         <div className="mt-5 mx-auto">
@@ -144,31 +182,11 @@ export default function Home() {
               className="cursor-pointer md:transition ease-in-out delay-150 md:hover:scale-150 duration-300 m-1 bg-white border border-yellow-500 rounded-lg max-w-sm"
               alt="..."
             />
-            <img
-              src={Foto1}
-              className={foto1}
-              alt="..."
-            />
-            <img
-              src={Foto2}
-              className={foto2}
-              alt="..."
-            />
-            <img
-              src={Foto3}
-              className={foto3}
-              alt="..."
-            />
-            <img
-              src={Foto4}
-              className={foto4}
-              alt="..."
-            />
-            <img
-              src={Foto5}
-              className={foto5}
-              alt="..."
-            />
+            <img src={Foto1} className={foto1} alt="..." />
+            <img src={Foto2} className={foto2} alt="..." />
+            <img src={Foto3} className={foto3} alt="..." />
+            <img src={Foto4} className={foto4} alt="..." />
+            <img src={Foto5} className={foto5} alt="..." />
           </div>
         </div>
 
@@ -190,19 +208,12 @@ export default function Home() {
                       as="h3"
                       className="text-lg font-bold text-center text-black"
                     >
-
-                      {state == false ? (
-                        'Not this time 😓'
-                      ) : ('Boa campeão! 😍')
-                      }
+                      {state == false ? "Not this time 😓" : "Boa campeão! 😍"}
                     </Dialog.Title>
-
 
                     <div className="mt-4 text-center">
                       <Link href="/">
-                        <button
-                          className="inline-flex justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
-                        >
+                        <button className="inline-flex justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2">
                           Cabô
                         </button>
                       </Link>
@@ -215,6 +226,5 @@ export default function Home() {
         </Transition>
       </div>
     </Container>
-  )
+  );
 }
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,79 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 /* eslint-disable @next/next/no-img-element */
-import Container from '../components/Container';
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Container from "../components/Container";
 
 export default function Entry() {
+  const [modal, setModal] = useState<any>(false);
+
+  function closeModal() {
+    setModal(false);
+  }
+
+  function openModal() {
+    setModal(true);
+  }
 
   return (
     <Container title="Filminho">
-    <div className="flex flex-col w-full h-full">
-      <div className="flex flex-col align-middle items-center justify-center my-20 mx-auto">
-        <img
-          src="movie.png"
-          className='w-64'
-          alt="..."
-        />
-
-        <img
-          src="logo.png"
-          className='w-64'
-          alt="..."
-        />
-
-        <a href="/home">
-          <button 
-            className="text-white font-medium p-0 w-20 h-20 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
+      <div className="flex flex-col w-full h-full">
+        <div className="fixed top-5 left-5">
+          <button
+            onClick={openModal}
+            className="p-0 w-12 h-12 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
           >
-            Lesgo!
+            <img src={"question.png"} className="ml-3 w-6 h-6" alt="..." />
           </button>
-        </a>
+        </div>
+        <div className="flex flex-col align-middle items-center justify-center my-20 mx-auto">
+          <img src="movie.png" className="w-64" alt="..." />
+
+          <img src="logo.png" className="w-64" alt="..." />
+
+          <a href="/home">
+            <button className="text-white font-medium p-0 w-20 h-20 bg-yellow-600 rounded-full hover:bg-yellow-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+              Lesgo!
+            </button>
+          </a>
+        </div>
       </div>
-    </div>
+
+      <Transition appear show={modal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="z-50 absolute w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg mb-5 font-bold text-center text-black"
+                  >
+                    Filminho Project
+                  </Dialog.Title>
+                  <p className="text-ellipsis">É um game de advinhação para filmes, todo dia teremos um filme novo com 6 frames diferentes, tente acertar se for capaz 😉</p>
+                  <div className="mt-4 text-center">
+                    <button
+                      onClick={closeModal}
+                      className="inline-flex justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2"
+                    >
+                      Voltar
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </Container>
-
-  )
+  );
 }
-
