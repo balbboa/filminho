6418b13b5d8c2423e8b521389da8545ea8a249e5
balbboa <abmm_arthur@hotmail.com>
2022-06-14 09:51:02 -0300

    Feat: correct bug replacing link by <a>

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,6 +5,7 @@ import { Fragment, useState } from 'react';
 import Container from '../components/Container';
 
 export default function Home() {
+  let cont = 0
 
   let movieSearchBox: null | any = ''
   let searchList: any = ''
@@ -44,6 +45,8 @@ export default function Home() {
 
   const callImg = async (e: any) => {
     e.preventDefault();
+    cont++
+    sessionStorage.setItem(`try${[cont]}`, e.target.elements.filme.value)
 
     if (e.target.elements.filme.value !== 'The Big Lebowski') { await contImg() }
     else {
@@ -82,9 +85,6 @@ export default function Home() {
     const URL = `https://api.themoviedb.org/3/search/movie?api_key=0c7861c0f9f582ae2a9ca7772ea92061&query=${searchMovie}`
     const res = await fetch(`${URL}`)
     const data = await res.json()
-
-    console.log(data.results)
-
     listMovies(data.results)
   }
 
@@ -100,7 +100,7 @@ export default function Home() {
     searchList.innerHTML = ''
     for (let i = 0; i < movies?.length; i++) {
       let movieListItem = document.createElement('div');
-      movieListItem.dataset.id = movies[i].id; // setting movie id in  data-id
+      movieListItem.dataset.id = movies[i].id; 
       movieListItem.classList.add('search-list-item');
 
       movieListItem.innerHTML = `
@@ -133,7 +133,7 @@ export default function Home() {
             <input onKeyUp={findMovies} type="search" id="idfilme" name="filme" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-yellow-500 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-500 dark:focus:border-yellow-500" placeholder="Digite o filme do dia" required />
             <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Xama</button>
           </div>
-          <div className="text-sm absolute" id="search-list">
+          <div className="text-sm w-1/3 absolute" id="search-list">
           </div>
         </form>
 
@@ -185,7 +185,7 @@ export default function Home() {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Panel className="z-50 absolute w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                     <Dialog.Title
                       as="h3"
                       className="text-lg font-bold text-center text-black"
